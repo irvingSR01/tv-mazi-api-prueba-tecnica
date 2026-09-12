63fce49f4a0d5e6e9b3ab9a59c2baf14d5e99a1e
Irving Roldán <67338300+irvingSR01@users.noreply.github.com>
2026-09-11 19:12:57 -0600

    chore: commit initializing base project

---
+++ src/test/java/com/irvingSR01/tv_maze_api/TvMazeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.irvingSR01.tv_maze_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TvMazeApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
